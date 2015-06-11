dd7a9265dd470bbf9b8a40e077ca0a1a05369b2c
Mix <mm5623s@ju09c-01c03-2.ad.fh-aachen.de>
2015-06-11 15:56:03 +0200

    Grundgerüst für Kommunikation mit dem WebService erstellt (NetworkCommunication class)

--- app/src/main/java/com/example/android/indoornavigation/networkcommunication/NetworkCommunication.java
+++ app/src/main/java/com/example/android/indoornavigation/networkcommunication/NetworkCommunication.java
@@ -1,7 +1,90 @@
 package com.example.android.indoornavigation.networkcommunication;
 
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+
 /**
  * Created by mm5623s on 11.06.15.
  */
 public class NetworkCommunication {
+    public NetworkCommunication() {
+        super();
+    }
+
+    public static String getLocation(){
+        String url = "https://testsystem.navi.fh-aachen.de/api/locate";
+
+        InputStream inputStream = null;
+        String result = "";
+        try {
+
+            // create HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+
+            // create POST request to the given URL
+            HttpPost httpPost = new HttpPost(url);
+
+            // add JSON data as String to the POST request
+            JSONObject json = new JSONObject();
+            json.put("test", true);
+            StringEntity se = new StringEntity( "JSON: " + json.toString());
+            httpPost.setEntity(se);
+            httpPost.setHeader("Content-type", "application/json");
+
+            // execute the POST request and receive response object
+            HttpResponse httpResponse = httpclient.execute(httpPost);
+
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if(inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+    }
+
+    public JSONObject convertStringToJSON(String result) {
+        JSONObject json = null;
+        try {
+            json = new JSONObject(result);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return json;
+    }
 }
